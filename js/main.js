4723e1370a178e242474d0ba292da56b3def4267
ChrisAK <ckennelly8910@gmail.com>
2017-07-14 14:52:51 -0400

    WAY MORE GAME LOGIC

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "Queen",
+    suit: "Hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "Queen",
+    suit: "Diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "King",
+    suit: "Hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "King",
+    suit: "Diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
@@ -11,8 +32,9 @@ var checkForMatch = function() {
   }
 }
 var flipCard = function(cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+cardsInPlay.push(cards[cardId].rank);
 checkForMatch();
 };
 flipCard(0);
