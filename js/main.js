800148f88d4745b6404df131204a7fdfe2803b6f
ChrisAK <ckennelly8910@gmail.com>
2017-07-16 18:13:38 -0400

    final

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var cards = [
   }
 ];
 var cardsInPlay = [];
+
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -31,11 +32,23 @@ var checkForMatch = function() {
     }
   }
 }
-var flipCard = function(cardId) {
+
+var flipCard = function() {
+var cardId = this.getAttribute('data-id');
 console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
 console.log(cards[cardId].cardImage);
 cardsInPlay.push(cards[cardId].rank);
+this.setAttribute('src', cards[cardId].cardImage);
 checkForMatch();
 };
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  };
+}
+createBoard();
